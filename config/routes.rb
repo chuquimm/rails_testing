822c9da3451b25106a73201405a13c3745510072
Marcos Chuquicondor <marcos.chuquicondor@gmail.com>
2019-12-19 12:09:30 -0500

    paths in nav

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   extend CropsRoutes
+  root 'crops#index'
   resources :type_products
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
